401adbb1ce1f6dbc5f6a6eb38d1404d175fb3001
Kirsten Comandich <kcomandich@gmail.com>
2019-03-05 09:57:12 -0800

    Add Robot.handle_command which is beginning to handle the PLACE and REPORT commands.

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,6 +1,7 @@
 require 'readline'
-
-FACING = %i( north south east west )
+QUIT_COMMANDS = %w[ quit exit ]
+VALID_COMMANDS = %w[ quit place move left right report ]
+FACING = %i[ north south east west ]
 RED = "\e[31m%s\e[0m"
 
 class Robot
@@ -20,7 +21,29 @@ class Robot
     command = nil
     while command != 'quit'
       command = Readline.readline(" ", true)
-      break if %w[ quit exit ].include? command
+      break if QUIT_COMMANDS.include? command.downcase
+
+      handle_command(command)
+    end
+  end
+
+  def handle_command(full_command)
+    command = full_command.split[0].downcase
+    args = full_command.split[1]
+    unless VALID_COMMANDS.include? command
+      STDERR.puts Robot.error 'Invalid option'
+      return
+    end
+
+    case command
+    when 'place'
+      # TODO error if no args
+      @x_coord = args.split(',')[0]
+      @y_coord = args.split(',')[1]
+      # TODO error checking for allowed coordinates
+      @facing = args.split(',')[2]
+    when 'report'
+      puts "X: #{@x_coord} Y: #{@y_coord}, Facing: #{@facing}"
     end
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Robot do
       allow(Readline).to receive(:readline).exactly(1).times.and_return('quit')
       r = Robot.new
     end
+    
+    it 'reports the coordinates and facing position' do
+      allow(Readline).to receive(:readline).exactly(3).times.and_return('PLACE 1,2,NORTH', 'REPORT', 'quit')
+      r = Robot.new
+      expect{r.accept_commands}.to output(/X: 1 Y: 2, Facing: NORTH/).to_stdout 
+    end
   end
 end
