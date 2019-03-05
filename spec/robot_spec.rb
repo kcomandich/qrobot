de23b49dd926223d885687dd9580e428b774775b
Kirsten Comandich <kcomandich@gmail.com>
2019-03-05 10:08:22 -0800

    Split the REPORT spec into two - separate out the test for PLACE logic.

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -7,11 +7,23 @@ RSpec.describe Robot do
       allow(Readline).to receive(:readline).exactly(1).times.and_return('quit')
       r = Robot.new
     end
-    
-    it 'reports the coordinates and facing position' do
-      allow(Readline).to receive(:readline).exactly(3).times.and_return('PLACE 1,2,NORTH', 'REPORT', 'quit')
+
+    it 'remembers the placed position' do
+      allow(Readline).to receive(:readline).exactly(2).times.and_return('PLACE 0,4,SOUTH', 'quit')
       r = Robot.new
-      expect{r.accept_commands}.to output(/X: 1 Y: 2, Facing: NORTH/).to_stdout 
+      r.accept_commands
+      expect(r.x_coord).to eq('0')
+      expect(r.y_coord).to eq('4')
+      expect(r.facing).to eq('SOUTH')
+    end
+
+    it 'reports the coordinates and facing position' do
+      allow(Readline).to receive(:readline).exactly(2).times.and_return('REPORT', 'quit')
+      r = Robot.new
+      r.x_coord = 1
+      r.y_coord = 2
+      r.facing = 'NORTH'
+      expect{r.accept_commands}.to output(/X: 1 Y: 2, Facing: NORTH/).to_stdout
     end
   end
 end
